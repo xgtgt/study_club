e73e18e42b72b63f099a291a445ec710019eff94
Guotao Xie <xieguotao0429@outlook.com>
2024-08-16 03:12:45 +1000

    题目标签、题目分类、新增题目接口开发

---
+++ study-club-subject/study-club-application/study-club-application-controller/target/generated-sources/annotations/com/studyclub/subjcet/application/convert/SubjectAnswerDTOConverterImpl.java
@@ -0,0 +1,44 @@
+package com.studyclub.subjcet.application.convert;
+
+import com.studyclub.subjcet.application.dto.SubjectAnswerDTO;
+import com.studyclub.subject.domain.entity.SubjectAnswerBO;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-08-16T02:57:54+1000",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_422 (Amazon.com Inc.)"
+)
+public class SubjectAnswerDTOConverterImpl implements SubjectAnswerDTOConverter {
+
+    @Override
+    public SubjectAnswerBO convertDTOBO(SubjectAnswerDTO subjectAnswerDTO) {
+        if ( subjectAnswerDTO == null ) {
+            return null;
+        }
+
+        SubjectAnswerBO subjectAnswerBO = new SubjectAnswerBO();
+
+        subjectAnswerBO.setOptionType( subjectAnswerDTO.getOptionType() );
+        subjectAnswerBO.setOptionContent( subjectAnswerDTO.getOptionContent() );
+        subjectAnswerBO.setIsCorrect( subjectAnswerDTO.getIsCorrect() );
+
+        return subjectAnswerBO;
+    }
+
+    @Override
+    public List<SubjectAnswerBO> convertListDTOToBO(List<SubjectAnswerDTO> dtoList) {
+        if ( dtoList == null ) {
+            return null;
+        }
+
+        List<SubjectAnswerBO> list = new ArrayList<SubjectAnswerBO>( dtoList.size() );
+        for ( SubjectAnswerDTO subjectAnswerDTO : dtoList ) {
+            list.add( convertDTOBO( subjectAnswerDTO ) );
+        }
+
+        return list;
+    }
+}
---
+++ study-club-subject/study-club-application/study-club-application-controller/target/generated-sources/annotations/com/studyclub/subjcet/application/convert/SubjectInfoDTOConverterImpl.java
@@ -0,0 +1,141 @@
+package com.studyclub.subjcet.application.convert;
+
+import com.studyclub.subjcet.application.dto.SubjectAnswerDTO;
+import com.studyclub.subjcet.application.dto.SubjectInfoDTO;
+import com.studyclub.subject.domain.entity.SubjectAnswerBO;
+import com.studyclub.subject.domain.entity.SubjectInfoBO;
+import com.studyclub.subject.infra.basic.entity.SubjectInfo;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-08-16T02:57:54+1000",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_422 (Amazon.com Inc.)"
+)
+public class SubjectInfoDTOConverterImpl implements SubjectInfoDTOConverter {
+
+    @Override
+    public SubjectInfoBO convertDTOToBO(SubjectInfoDTO subjectInfoDTO) {
+        if ( subjectInfoDTO == null ) {
+            return null;
+        }
+
+        SubjectInfoBO subjectInfoBO = new SubjectInfoBO();
+
+        subjectInfoBO.setId( subjectInfoDTO.getId() );
+        subjectInfoBO.setSubjectName( subjectInfoDTO.getSubjectName() );
+        subjectInfoBO.setSubjectDifficult( subjectInfoDTO.getSubjectDifficult() );
+        subjectInfoBO.setSettleName( subjectInfoDTO.getSettleName() );
+        subjectInfoBO.setSubjectType( subjectInfoDTO.getSubjectType() );
+        subjectInfoBO.setSubjectScore( subjectInfoDTO.getSubjectScore() );
+        subjectInfoBO.setSubjectParse( subjectInfoDTO.getSubjectParse() );
+        subjectInfoBO.setSubjectAnswer( subjectInfoDTO.getSubjectAnswer() );
+        List<Integer> list = subjectInfoDTO.getCategoryIds();
+        if ( list != null ) {
+            subjectInfoBO.setCategoryIds( new ArrayList<Integer>( list ) );
+        }
+        List<Integer> list1 = subjectInfoDTO.getLabelIds();
+        if ( list1 != null ) {
+            subjectInfoBO.setLabelIds( new ArrayList<Integer>( list1 ) );
+        }
+        List<String> list2 = subjectInfoDTO.getLabelName();
+        if ( list2 != null ) {
+            subjectInfoBO.setLabelName( new ArrayList<String>( list2 ) );
+        }
+        subjectInfoBO.setOptionList( subjectAnswerDTOListToSubjectAnswerBOList( subjectInfoDTO.getOptionList() ) );
+        subjectInfoBO.setCategoryId( subjectInfoDTO.getCategoryId() );
+        subjectInfoBO.setLabelId( subjectInfoDTO.getLabelId() );
+        subjectInfoBO.setKeyWord( subjectInfoDTO.getKeyWord() );
+        subjectInfoBO.setCreateUser( subjectInfoDTO.getCreateUser() );
+        subjectInfoBO.setCreateUserAvatar( subjectInfoDTO.getCreateUserAvatar() );
+        subjectInfoBO.setSubjectCount( subjectInfoDTO.getSubjectCount() );
+        subjectInfoBO.setLiked( subjectInfoDTO.getLiked() );
+        subjectInfoBO.setLikedCount( subjectInfoDTO.getLikedCount() );
+        subjectInfoBO.setNextSubjectId( subjectInfoDTO.getNextSubjectId() );
+        subjectInfoBO.setLastSubjectId( subjectInfoDTO.getLastSubjectId() );
+
+        return subjectInfoBO;
+    }
+
+    @Override
+    public SubjectInfo convertBoToInfo(SubjectInfoBO subjectInfoBO) {
+        if ( subjectInfoBO == null ) {
+            return null;
+        }
+
+        SubjectInfo subjectInfo = new SubjectInfo();
+
+        subjectInfo.setId( subjectInfoBO.getId() );
+        subjectInfo.setSubjectName( subjectInfoBO.getSubjectName() );
+        subjectInfo.setSubjectDifficult( subjectInfoBO.getSubjectDifficult() );
+        subjectInfo.setSettleName( subjectInfoBO.getSettleName() );
+        subjectInfo.setSubjectType( subjectInfoBO.getSubjectType() );
+        subjectInfo.setSubjectScore( subjectInfoBO.getSubjectScore() );
+        subjectInfo.setSubjectParse( subjectInfoBO.getSubjectParse() );
+        subjectInfo.setSubjectCount( subjectInfoBO.getSubjectCount() );
+
+        return subjectInfo;
+    }
+
+    @Override
+    public List<SubjectInfoBO> convertListInfoToBO(List<SubjectInfo> subjectInfoList) {
+        if ( subjectInfoList == null ) {
+            return null;
+        }
+
+        List<SubjectInfoBO> list = new ArrayList<SubjectInfoBO>( subjectInfoList.size() );
+        for ( SubjectInfo subjectInfo : subjectInfoList ) {
+            list.add( subjectInfoToSubjectInfoBO( subjectInfo ) );
+        }
+
+        return list;
+    }
+
+    protected SubjectAnswerBO subjectAnswerDTOToSubjectAnswerBO(SubjectAnswerDTO subjectAnswerDTO) {
+        if ( subjectAnswerDTO == null ) {
+            return null;
+        }
+
+        SubjectAnswerBO subjectAnswerBO = new SubjectAnswerBO();
+
+        subjectAnswerBO.setOptionType( subjectAnswerDTO.getOptionType() );
+        subjectAnswerBO.setOptionContent( subjectAnswerDTO.getOptionContent() );
+        subjectAnswerBO.setIsCorrect( subjectAnswerDTO.getIsCorrect() );
+
+        return subjectAnswerBO;
+    }
+
+    protected List<SubjectAnswerBO> subjectAnswerDTOListToSubjectAnswerBOList(List<SubjectAnswerDTO> list) {
+        if ( list == null ) {
+            return null;
+        }
+
+        List<SubjectAnswerBO> list1 = new ArrayList<SubjectAnswerBO>( list.size() );
+        for ( SubjectAnswerDTO subjectAnswerDTO : list ) {
+            list1.add( subjectAnswerDTOToSubjectAnswerBO( subjectAnswerDTO ) );
+        }
+
+        return list1;
+    }
+
+    protected SubjectInfoBO subjectInfoToSubjectInfoBO(SubjectInfo subjectInfo) {
+        if ( subjectInfo == null ) {
+            return null;
+        }
+
+        SubjectInfoBO subjectInfoBO = new SubjectInfoBO();
+
+        subjectInfoBO.setId( subjectInfo.getId() );
+        subjectInfoBO.setSubjectName( subjectInfo.getSubjectName() );
+        subjectInfoBO.setSubjectDifficult( subjectInfo.getSubjectDifficult() );
+        subjectInfoBO.setSettleName( subjectInfo.getSettleName() );
+        subjectInfoBO.setSubjectType( subjectInfo.getSubjectType() );
+        subjectInfoBO.setSubjectScore( subjectInfo.getSubjectScore() );
+        subjectInfoBO.setSubjectParse( subjectInfo.getSubjectParse() );
+        subjectInfoBO.setSubjectCount( subjectInfo.getSubjectCount() );
+
+        return subjectInfoBO;
+    }
+}
