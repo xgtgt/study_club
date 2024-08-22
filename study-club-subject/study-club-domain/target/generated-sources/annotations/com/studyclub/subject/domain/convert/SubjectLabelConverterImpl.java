dc18c6338c65708e359b1ee02c777d3f1acc5c9c
Guotao Xie <xieguotao0429@outlook.com>
2024-08-22 18:05:33 +1000

    0822

--- study-club-subject/study-club-domain/target/generated-sources/annotations/com/studyclub/subject/domain/convert/SubjectLabelConverterImpl.java
+++ study-club-subject/study-club-domain/target/generated-sources/annotations/com/studyclub/subject/domain/convert/SubjectLabelConverterImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-20T17:17:27+1000",
+    date = "2024-08-22T18:02:03+1000",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_422 (Amazon.com Inc.)"
 )
 public class SubjectLabelConverterImpl implements SubjectLabelConverter {
